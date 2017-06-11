a1a61110367c043ea2fd1431d2c3d93e8a8901af
Nickolai Novik <nickolainovik@gmail.com>
2017-06-11 21:29:01 +0300

    update __init__

--- aioc/__init__.py
+++ aioc/__init__.py
@@ -1,7 +1,5 @@
 from .state import encode_message, decode_message
-from .cluster import Cluster
-from .config import Config, LAN
 
 
 __version__ = '0.0.1a0'
-__all__ = ('encode_message', 'decode_message', 'Cluster', 'Config', 'LAN')
+__all__ = ('encode_message', 'decode_message')
