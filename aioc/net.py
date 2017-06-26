df04e9384281dc7c4fa93d7c3f7a16c5b7faa2ea
Nickolai Novik <nickolainovik@gmail.com>
2017-06-27 00:28:14 +0300

    update networking

--- aioc/net.py
+++ aioc/net.py
@@ -1,12 +1,11 @@
 import asyncio
 
-from aioc import state
-from aioc.state import add_msg_size, decode_msg_size, LENGTH_SIZE
+from . import state
 
 
 async def read_message(reader):
-    size_data = await reader.readexactly(LENGTH_SIZE)
-    mg_size = decode_msg_size(size_data)
+    size_data = await reader.readexactly(state.LENGTH_SIZE)
+    mg_size = state.decode_msg_size(size_data)
     raw_message = await reader.readexactly(mg_size)
     msg = state.decode_message(raw_message)
     return msg
@@ -23,13 +22,13 @@ class Connection:
     async def _read_data(self):
         while not self._closing:
             msg = await read_message(self._reader)
-            print("---------------")
+            print(Connection, "got message")
             print(msg)
             await self._manager.queue.put(msg)
 
     def send(self, message):
         raw_payload = state.encode_message(message)
-        self._writer.write(add_msg_size(raw_payload))
+        self._writer.write(state.add_msg_size(raw_payload))
 
     async def close(self):
         self._closing = True
@@ -42,11 +41,12 @@ class Connection:
 
 class ConnectionManager:
 
-    def __init__(self, local_node):
+    def __init__(self, local_node, config):
         self._connections = {}
         self._queue = asyncio.Queue()
         self._closing = True
         self._local_node = local_node
+        self._config = config
 
     @property
     def queue(self):
@@ -61,18 +61,21 @@ class ConnectionManager:
         hello = await read_message(reader)
         if not isinstance(hello, state.Hello):
             data = b"bad init line"
-            writer.write(add_msg_size(data))
+            writer.write(state.add_msg_size(data))
             await writer.drain()
             writer.close()
             return
+
+        #assert hello.cluster_id == self._config.cluster_id
 
         conn = Connection(reader, writer, self)
         self._connections[hello.sender] = conn
 
     async def _create_connection(self, node):
         reader, writer = await asyncio.open_connection(node.host, node.port)
-        data = state.encode_message(state.Hello(1, self._local_node))
-        writer.write(add_msg_size(data))
+        msg = state.Hello(self._config.cluster_id, self._local_node)
+        data = state.encode_message(msg)
+        writer.write(state.add_msg_size(data))
         await writer.drain()
         conn = Connection(reader, writer, self)
         self._connections[node] = conn
