15eee084368ca515da8fdca7c27ca34d6d30123f
Nikolay Novik <nickolainovik@gmail.com>
2017-11-06 22:29:33 +0200

    update config

--- aioc/config.py
+++ aioc/config.py
@@ -5,8 +5,8 @@ class _Config(NamedTuple):
     host: str
     port: int
     join_timeout: float = 10
-    push_pull_interval: float = 15
-    gossip_interval: float = 0.25
+    push_pull_interval: float = 3
+    gossip_interval: float = 1
     gossip_nodes: int = 1
     probe_interval: float = 5
     retransmit_mult: int = 2
@@ -22,4 +22,17 @@ class Config(_Config):
         return self._replace(**kw)
 
 
-LAN = Config('localhost', 50001, 10, 15, 0.25, 1, 5, 2, 3600, 1, 1, 1)
+LAN = Config(
+    host='localhost',
+    port=50001,
+    join_timeout=10,
+    push_pull_interval=15,
+    gossip_interval=0.25,
+    gossip_nodes=1,
+    probe_interval=5,
+    retransmit_mult=2,
+    gossip_to_dead=3600,
+    probe_timeout=1,
+    suspicion_mult=1,
+    suspicion_max_timeout_mult= 1
+)
