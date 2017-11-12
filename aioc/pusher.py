cd24e5a87439b9ae39bcbdb7dfd815629004b2fe
Nikolay Novik <nickolainovik@gmail.com>
2017-11-12 17:46:56 +0200

    update push pull

--- aioc/pusher.py
+++ aioc/pusher.py
@@ -1,4 +1,5 @@
-from .state import PushPull, ALIVE
+from .state import PushPull
+import time
 
 
 __all__ = ('Pusher',)
@@ -15,18 +16,27 @@ class Pusher:
     async def push_pull_address(self, address):
         metas = list(self._mlist._members.values())
         msg = PushPull(self._mlist.local_node, metas, True)
-        resp = await self._tcp.send_message(address, msg)
-        self._gossiper.merge(resp)
+        try:
+            # TODO: add timeout
+            resp = await self._tcp.send_message(address, msg)
+        except OSError as e:
+            print(e)
+        else:
+            self._gossiper.merge(resp)
 
     async def push_pull(self):
-        def filter_func(node_meta):
-            if node_meta.node == self._mlist.local_node:
-                return False
-            if node_meta.status != ALIVE:
-                return False
-            return True
-        node_meta = self._mlist.kselect(1, filter_func)
-        await self.push_pull_address(node_meta.node)
+        """push_pull is invoked periodically to randomly perform a complete state
+        exchange. Used to ensure a high level of convergence, but is also
+        reasonably expensive as the entire state of this node is exchanged
+        with the other node.
+        """
+
+        def f(meta):
+            return not meta.is_local
+
+        metas = self._mlist.kselect(1, f)
+        for meta in metas:
+            await self.push_pull_address(meta.node)
 
     async def join(self, *hosts) -> int:
         success = 0
